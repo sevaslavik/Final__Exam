046736e3f58feebcfbc92eef9bf404d2dd1e202c
sevaslavik <seva5000Pro@gmail.com>
2019-07-10 19:04:03 +0300

    section with slider

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 "use strict";
+/*
+;(function($){
 
+    $('.js-slider').slick({
+        arrows: false,
+        dots: true,
+        //appendDots: '.dots',
+        appendDots: $('.dots'),
+        customPaging : function(slider, i) {
+            return '<span class="slider-dot"></span>';
+        },
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    })
+
+}(jQuery));*/
